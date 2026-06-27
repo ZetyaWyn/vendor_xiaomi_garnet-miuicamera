49c6ec9fa1ec12e9137a349968cc0d12230ae2cc
Jotanh <jotanh@jotanic.xyz>
2026-06-27 02:40:23 -0300

    Initial

---
+++ garnet-miuicamera-vendor.mk
@@ -0,0 +1,14 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/xiaomi/garnet-miuicamera
+
+PRODUCT_PACKAGES += \
+    libcamera_algoup_jni.xiaomi \
+    libcamera_mianode_jni.xiaomi \
+    libmicampostproc_client \
+    vendor.xiaomi.hardware.campostproc@1.0_system \
+    MiuiCamera \
+    MiuiScanner
